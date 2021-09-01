ca584a358cae7772c7243eedc089862c51b1d5b0
lijianguo <lijianguo@zhumengyuan.com>
2021-09-01 16:37:06 +0800

    chore(addons): modify Deprecating API authentication through query parameters

--- scripts/push_release.sh
+++ scripts/push_release.sh
@@ -11,16 +11,16 @@ GIT_REPO=$2
 
 CREATE_RELEASE_DATA='{
   "tag_name": "'"${GIT_TAG}"'",
-  "target_commitish": "master",
+  "target_commitish": "main",
   "name": "'"${GIT_TAG}"'",
   "body": "release-'"${GIT_TAG}"'",
   "draft": false,
   "prerelease": false
   }'
 
-echo "Creating a new release: $GIT_TAG branch: master"
+echo "Creating a new release: $GIT_TAG branch: main"
 
-RESPONSE=$(curl -s --data "${CREATE_RELEASE_DATA}" "https://api.github.com/repos/$GIT_REPO/releases?access_token=${GITHUB_TOKEN}")
+RESPONSE=$(curl -s --data "${CREATE_RELEASE_DATA}" -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/$GIT_REPO/releases")
 ASSET_UPLOAD_URL=$(echo "$RESPONSE" | jq -r .upload_url | cut -d '{' -f1)
 if [ -z "$ASSET_UPLOAD_URL" ]; then
     echo ${RESPONSE}
@@ -29,5 +29,5 @@ fi
 
 for FILE in toCopy/*; do
     echo "Uploading asset: $FILE to url: $ASSET_UPLOAD_URL?name=${FILE}"
-    curl -s --data-binary @${FILE} -H "Content-Type: application/octet-stream" -X POST "$ASSET_UPLOAD_URL?name=$(basename ${FILE})&access_token=${GITHUB_TOKEN}"
+    curl -s --data-binary @${FILE} -H "Content-Type: application/octet-stream" -H "Authorization: token ${GITHUB_TOKEN}" -X POST "$ASSET_UPLOAD_URL?name=$(basename ${FILE})"
 done
--- scripts/remove_latest_release.sh
+++ scripts/remove_latest_release.sh
@@ -4,14 +4,14 @@ set -e
 GIT_REPO=$1
 
 echo "Getting latest release from $GIT_REPO"
-RESP=$(curl -s "https://api.github.com/repos/$GIT_REPO/releases/latest?access_token=${GITHUB_TOKEN}")
+RESP=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/$GIT_REPO/releases/latest")
 URLS=$(echo "$RESP" | jq -r .url)
-if [ -z "${URLS}" ]; then
+if [ "${URLS}" ] && [ -z "${URLS}" ]; then
     echo ${RESP}
     exit 1
 fi
 RELEASE_URL=(${URLS// / })
 
 echo "Deleting old latest release"
-RESPONSE=$(curl -s -X DELETE "${RELEASE_URL}?access_token=${GITHUB_TOKEN}")
+RESPONSE=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" -X DELETE "${RELEASE_URL}")
 echo ${RESPONSE}
