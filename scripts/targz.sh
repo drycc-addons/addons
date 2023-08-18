b5f6f2526b29134c46383638e4a6de72d82714d3
lijianguo <lijianguo1991@outlook.com>
2023-08-18 11:01:16 +0800

    chore(addons): modify targz tgz

--- scripts/targz.sh
+++ scripts/targz.sh
@@ -6,6 +6,6 @@ cd $source_dir
 for addon in *; do
   if [[ -d $addon ]]; then
     addon=$(echo $addon | awk -F '/' '{print $NF}')
-    tar -czvf ../$destination_dir/$addon.tar.gz $addon
+    tar -czvf ../$destination_dir/$addon.tgz $addon
   fi
 done
