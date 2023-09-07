883d78f36367680f27ab97d3968529ef70d2190e
lijianguo <lijianguo1991@outlook.com>
2023-09-07 15:43:40 +0800

    chore(addons): index.yaml add created digest info

--- scripts/push_release.sh
+++ scripts/push_release.sh
@@ -27,6 +27,22 @@ if [ -z "$ASSET_UPLOAD_URL" ]; then
     exit 1
 fi
 
+# modify index.yaml, add created\digest field
+if [ ! -f toCopy/index.yaml ]; then
+    echo "index.yaml does not exist"
+    exit 1
+fi
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+for FILE in `ls toCopy/*.tgz`; do
+    sha=$(sha256sum $FILE | awk '{ print $1 }')  # FILE Example: toCopy/minio-2023.tgz
+    FILE=${FILE%%.tgz*}  # remove ".tgz", Example: toCopy/minio-2023
+    addonVersion=${FILE##*toCopy/}  # remove "path, Example: minio-2023
+    addon=${addonVersion%-*}  # remove after "-" , Example: minio
+    version=${addonVersion##*-}  # remove before "-", Example: 2023
+    a=$addon v=$version t=$timestamp yq -i '(.entries.[env(a)][] | select(.version == env(v)) | .created) = env(t)' toCopy/index.yaml
+    a=$addon v=$version s=$sha yq -i '(.entries.[env(a)][] | select(.version == env(v)) | .digest) = env(s)' toCopy/index.yaml
+done
+
 for FILE in toCopy/*; do
     echo "Uploading asset: $FILE to url: $ASSET_UPLOAD_URL?name=${FILE}"
     curl -s --data-binary @${FILE} -H "Content-Type: application/octet-stream" -H "Authorization: token ${GITHUB_TOKEN}" -X POST "$ASSET_UPLOAD_URL?name=$(basename ${FILE})"
