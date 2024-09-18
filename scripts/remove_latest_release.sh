8e1e578bfdd20b4b6dd3baad47aed5f388d6ceab
duanhongyi <duanhongyi@doopai.com>
2024-09-18 09:29:31 +0800

    chore(woodpecker): add env export

--- scripts/remove_latest_release.sh
+++ scripts/remove_latest_release.sh
@@ -4,7 +4,7 @@ set -e
 GIT_REPO=$1
 
 echo "Getting latest release from $GIT_REPO"
-RESP=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/$GIT_REPO/releases/latest")
+RESP=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/$GIT_REPO/releases/tags/latest")
 URLS=$(echo "$RESP" | jq -r .url)
 if [ "${URLS}" ] && [ -z "${URLS}" ]; then
     echo ${RESP}
